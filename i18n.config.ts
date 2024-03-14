e3b870dc84181e371655b8963887df603fcac722
ndtien142 <ndtien142@gmail.com>
2024-03-14 16:30:43 +0700

    add library

---
+++ i18n.config.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import {en, vi} from './src/common/translations/translations';
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  vi: {
+    translation: vi,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  resources,
+  lng: 'vi',
+  // language to use if translations in user language are not available
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false, // not needed for react!!
+  },
+});
+
+export default i18n;
