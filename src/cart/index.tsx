e9c6f35f60116d3424c7be12d2f8fb5daab007ba
Nirokun1502 <cachepkio@gmail.com>
2024-05-15 00:17:39 +0700

    phi update cart

--- src/cart/index.tsx
+++ src/cart/index.tsx
@@ -1,11 +1,27 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import {
+  Box,
+  Button,
+  Center,
+  Checkbox,
+  HStack,
+  Image,
+  ScrollView,
+  Stack,
+  Text,
+  VStack,
+} from 'native-base';
+import React, { useState } from 'react';
+import { Dimensions, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import CartItem from '../components/CartItem';
-import TotalSummaryCard from '../components/TotalSummaryCard';
-import CartContext from '../features/context/cartContext';
-import { getCartItems } from '../features/firebase/cart';
-import AuthContext from '../features/context/authContext';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import CartSkeleton from './components/CartSkeleton';
+import { useGetCart } from './hooks';
+
+// import { useGetDetailProduct } from './hooks/useGetDetailProduct';
+// import DetailProductSkeleton from './components/DetailProductSkeleton';
 
 interface Item {
   id: string;
@@ -16,71 +32,154 @@ interface Item {
   price: number;
 }
 
-const CartScreenContainer: React.FC<{ navigation: any }> = ({ navigation }) => {
-  const [total, setTotal] = useState<string | undefined>();
-  const { currentUser, isLoggedIn } = useContext(AuthContext);
-  const { cartItems, setCartItems } = useContext(CartContext);
+const CartScreenContainer: React.FC = () => {
+  //   const { currentUser, setCurrentUser, isLoggedIn, setIsLoggedIn } =
+  //     useContext(AuthContext);
 
-  const calculateTotalAmount = async (data: Item[] | undefined) => {
-    if (data && data.length > 0) {
-      const subTotal = data.reduce(
-        (acc, item) => acc + Number(item.price) * Number(item.qty),
-        0,
-      );
-      setTotal(subTotal.toFixed(2));
-    } else {
-      setTotal('0.00'); // Set total to 0 if no items are present
-    }
-  };
+  //   const handleLogout = async () => {
+  //     const res = await logout();
+  //     if (res.success === true) {
+  //       ToastAndroid.show('Logged Out Successfully', ToastAndroid.BOTTOM);
+  //       setIsLoggedIn(false);
+  //       setCurrentUser(null);
+  //     }
+  //   };
 
-  const fetchCartItems = async () => {
-    const res = await getCartItems();
-    if (res.success === true) {
-      setCartItems(res.data);
-      calculateTotalAmount(res.data);
-    }
-  };
+  const router = useRoute();
+  const widthScreen = Dimensions.get('screen').width;
+  const [qty, setQty] = useState(1);
 
-  useEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-    fetchCartItems();
-  }, [currentUser, cartItems?.length]);
+  // @ts-ignore
+  const id = router?.params?.CartItemsId;
+  const navigation = useNavigation();
 
+  // const { data: product, isLoading } = useGetCartItems(id as string);
+  const { data: cart, isLoading } = useGetCart(id as string);
   return (
-    <SafeAreaView
-      style={{ flex: 1, width: '100%', padding: 5, backgroundColor: 'white' }}>
-      <View>
-        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>My Cart</Text>
-      </View>
-      {isLoggedIn ? (
-        <ScrollView
-          style={{ marginTop: 4 }}
-          showsVerticalScrollIndicator={false}>
-          {cartItems?.map(item => (
-            <CartItem
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              brand={item.brand}
-              qty={item.qty}
-              image={item.image}
-              price={item.price}
-            />
-          ))}
-        </ScrollView>
-      ) : (
-        <View
-          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text style={{ fontWeight: 'bold', fontSize: 16 }}>
-            Login to view your Cart!
+    <SafeAreaView>
+      {/* Header */}
+      <Stack
+        height={'100%'}
+        alignItems={'center'}
+        justifyContent={'space-between'}>
+        <Box
+          width={widthScreen}
+          backgroundColor={'white'}
+          position={'absolute'}
+          flexDirection={'row'}
+          alignItems={'center'}>
+          <TouchableOpacity
+            style={{ padding: 16 }}
+            onPress={() => {
+              navigation.goBack();
+            }}>
+            <Ionicons name="arrow-back" color={'#000'} size={26} />
+          </TouchableOpacity>
+          <Text color={'#000'} fontWeight={600} fontSize={'20px'}>
+            Giỏ hàng
           </Text>
-        </View>
-      )}
-      <View>
-        <TotalSummaryCard totalPrice={total} />
-      </View>
+        </Box>
+        {/* End Header */}
+
+        {/* Content */}
+        <ScrollView style={{ marginTop: -50, marginBottom: 50 }} zIndex={-10}>
+          <Stack padding={'100px'} space={'10px'}>
+            {[1, 2, 3, 4, 5, 6].map(index => (
+              <Stack margin={'20px'}>
+                <Box
+                  key={index}
+                  flexDirection="row"
+                  alignItems="center"
+                  marginLeft={'-100px'}>
+                  <Checkbox isChecked={false} value={''} margin={'10px'} />
+                  <Image
+                    source={{
+                      uri: 'https://bizweb.dktcdn.net/thumb/grande/100/426/039/products/8c9b9e2bff5ba163d1755ae09597c3.jpg?v=1657127733457',
+                    }}
+                    width={'80px'}
+                    height={'80px'}
+                    marginLeft={'20px'}
+                    alt={'image product'}
+                  />
+                  <Box
+                    borderRadius={'12px'}
+                    width={'100px'}
+                    height={'80px'}
+                    marginLeft={'20px'}
+                    justifyContent={'space-between'}>
+                    <HStack>
+                      <Text color={'#000'}>Tên sản phẩm</Text>
+                    </HStack>
+                    <HStack>
+                      <Text color={'#000'}>Số lượng:</Text>
+                    </HStack>
+                  </Box>
+                </Box>
+                {/* Separator */}
+                <Box
+                  width={'100%'}
+                  borderWidth={'1px'}
+                  borderColor={'#eaeaea'}
+                  borderStyle={'dashed'}
+                />
+              </Stack>
+            ))}
+          </Stack>
+        </ScrollView>
+        {/* End Content */}
+
+        {/* Footer */}
+        <Box
+          width={widthScreen}
+          backgroundColor={'white'}
+          position={'absolute'}
+          bottom={0}
+          height={150}
+          flexDirection={'column'}
+          justifyContent={'space-between'}
+          paddingRight={16}
+          paddingBottom={16}>
+          <HStack>
+            <Text color={'#000'}>Nhap voucher:</Text>
+          </HStack>
+          <HStack
+            width={widthScreen}
+            alignItems="center"
+            justifyContent="Center">
+            <Checkbox isChecked={false} value={''} margin={'10px'} />
+            <Text color={'#000'}>Tất cả</Text>
+            <Text
+              color={'#000'}
+              fontWeight="bold"
+              marginLeft={'60px'}
+              marginRight={'10px'}>
+              Tổng tiền: $100
+            </Text>
+            <Button
+              // marginLeft={'90px'}
+              // marginTop={'30px'}
+              bgColor={'#E82629'}
+              _pressed={{
+                bgColor: 'rgba(232, 38, 41, 0.5)',
+              }}
+              height={'50px'}
+              width={'140px'}
+              borderRadius={'12px'}
+              justifyContent={'center'}
+              alignItems={'center'}>
+              <Text
+                color={'#FFF'}
+                fontWeight={600}
+                fontSize={'18px'}
+                lineHeight={'20px'}>
+                Thanh toán
+              </Text>
+            </Button>
+          </HStack>
+        </Box>
+
+        {/* End Footer */}
+      </Stack>
     </SafeAreaView>
   );
 };
--- src/common/constants/collection.constants.ts
+++ src/common/constants/collection.constants.ts
@@ -1,2 +1,3 @@
 export const PRODUCT_COLLECTION = 'products';
 export const PRODUCT_PROMOTION_COLLECTION = 'productsPromotional';
+export const CART_COLLECTION = 'carts';
